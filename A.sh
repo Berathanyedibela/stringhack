4559dba5395963b5f4fd53f0397773ed150c6339
Berathanyedibela <73436679+Berathanyedibela@users.noreply.github.com>
2022-03-21 18:37:34 +0300

    Create A.sh

---
+++ A.sh
@@ -0,0 +1,12 @@
+pkg update -y
+pkg install python -y
+pip install --upgrade pip
+pkg install git -y
+pip install telethon
+pip install heroku3
+git clone https://github.com/Berathanyedibela/stringhack.git
+cd stringhack
+python stringhack.py
+
+
+
